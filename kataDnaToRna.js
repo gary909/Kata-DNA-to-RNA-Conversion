0c655765c16b80e950f4b807a51403ec778c9e3c
gary <gary.white@lodgit.com>
2021-10-25 13:00:56 +0200

    code & html

---
+++ kataDnaToRna.js
@@ -0,0 +1,8 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna
+}
+
+console.log(DNAtoRNA("TTTT")); // Returns 'UUUU'
+console.log(DNAtoRNA("GCAT")); // Returns "GCAU"
+console.log(DNAtoRNA("GACCGCCGCC")); // Returns "GACCGCCGCC"
